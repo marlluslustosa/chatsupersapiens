a1bf06f1d512f8488d159c121f7b1b2146a9a6c8
Marllus Lustosa <2443853-mlustosa@users.noreply.gitlab.com>
2024-07-25 23:38:00 -0300

    consertando chamadas api

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/chat', (req, res) => {
+    const question = req.body.question;
+
+    const config = {
+        headers: {
+            "x-api-key": process.env.CHATPDF_API_KEY,
+            "Content-Type": "application/json",
+        },
+    };
+
+    const data = {
+        referenceSources: true,
+	sourceId: process.env.CHATPDF_SOURCE_ID,
+        messages: [
+            {
+                role: "user",
+                content: question,
+            },
+        ],
+    };
+
+    axios
+        .post("https://api.chatpdf.com/v1/chats/message", data, config)
+        .then((response) => {
+            res.json({ content: response.data.content });
+        })
+        .catch((error) => {
+            res.status(500).json({ error: error.message });
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
